7c1c8d1cbb996bd72546fb48270c230d9e939a7b
Rama Krishna Gorentla <62733@ctrls.in>
2024-02-12 14:14:13 +0530

    test

---
+++ introduction.js
@@ -0,0 +1,61 @@
+// let string = "JavaScript";
+
+// console.log(string.substring(0, 4));
+
+// console.log(2 + 3);
+
+// console.log(typeof null);
+
+// let age = 100; // age in years
+// const gravity = 9.81; // earth gravity  in m/s2
+// const boilingPoint = 100; // water boiling point, temperature in °C
+// const PI = 3.14; // geometrical constant
+// console.log(gravity, boilingPoint, PI);
+
+// let nums = [1, 2, 3]
+// nums[0] = 10;
+// console.log(nums);
+
+// const PI = Math.PI
+// console.log(PI)
+
+// console.log(Math.round(PI))
+
+// console.log(Math.sqrt(100));
+
+// let firstName = "rahul"
+// let lastName = "puhul"
+// console.log(`Iam ${firstName}`)
+
+// let num = "hello"
+// console.log(num.replace('hello', 'hey'))
+
+// let num = '10'
+// let numINT = parseInt(num)
+// console.log(numINT)
+
+// console.log(NaN === NaN);
+// console.log(3 === 3)
+
+// const now = new Date();
+// console.log(now);
+
+let num = 3
+if (num > 0) {
+    console.log("today is raining")
+}
+
+let weather = "cloudy";
+switch (weather) {
+  case "rainy":
+    console.log("You need a rain coat.");
+   
+  case "cloudy":
+    console.log("It might be cold, you need a jacket.");
+   
+  case "sunny":
+    console.log("Go out freely.");
+    break;
+  default:
+    console.log(" No need for rain coat.");
+}
